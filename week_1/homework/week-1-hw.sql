6fe389e9a3d328c49585dffa7dc6e7e770c7e436
Harshit Singh <harshitsingh.gbu@gmail.com>
2023-01-19 17:18:34 +0530

    week_1_hw

---
+++ week_1/homework/week-1-hw.sql
@@ -0,0 +1,49 @@
+--Q3 How many taxi trips were totally made on January 15?
+
+select count(1)
+from green_taxi_data t
+where
+	date(t.lpep_pickup_datetime) = '2019-01-15'
+	and
+	date(t.lpep_dropoff_datetime) = '2019-01-15'
+;
+
+--Q4 Which was the day with the largest trip distance Use the pick up time for your calculations.
+
+select
+	date(t.lpep_pickup_datetime) putime ,
+	max(t.trip_distance) maxdist
+from green_taxi_data t
+group by putime
+order by maxdist desc
+limit 1;
+
+--Q5 In 2019-01-01 how many trips had 2 and 3 passengers?
+
+select
+	format('2: %s ; 3: %s',
+	(select count(1)
+	from green_taxi_data
+	where
+		passenger_count = 2
+		and 
+		date(lpep_pickup_datetime) = '2019-01-01'),
+	(select count(1)
+	from green_taxi_data
+	where
+		passenger_count = 3
+		and 
+		date(lpep_pickup_datetime) = '2019-01-01')) ans
+;
+
+--Q6 For the passengers picked up in the Astoria Zone which was the drop off zone that had the largest tip? We want the name of the zone, not the id.
+
+select zdo."Zone"
+from green_taxi_data t
+join zones zpu on
+	t."PULocationID" = zpu."LocationID"
+join zones zdo on
+	t."DOLocationID" = zdo."LocationID"
+where zpu."Zone" = 'Astoria'
+order by t.tip_amount desc
+limit 1;
---
+++ week_1/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+
+# some fun stuff
+
+print(f'hi, looks like it worked for day = {day}')
